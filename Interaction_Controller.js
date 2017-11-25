20f21294d92fbe4a8567f820c5d406b2b70f2265
Yvonne Chen <yc.yvonnechen@yahoo.com>
2017-11-24 22:26:38 -0800

    removed console logs

--- Interaction_Controller.js
+++ Interaction_Controller.js
@@ -11,18 +11,6 @@ class Interaction_Controller {
 		let newX = this.player.curX + x;
 		let newZ = (this.player.curZ - 1) + z;
 		let obstacles = this.ground.getStripObstacles(newZ);
-
-		console.log('delta x', x)
-		console.log('delta z', z)
-
-		console.log('currentX', this.player.curX)
-		console.log('newX', newX)
-
-		console.log('currentZ', this.player.curZ - 1)
-		console.log('newZ', newZ)
-
-		console.log('obstacles', obstacles)
-
 		return obstacles.indexOf(newX) != -1;
 	}
 
